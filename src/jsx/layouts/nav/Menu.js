ca2747b800706aed3c7c758e34a95836682d4456
Mohit <20bma048@nith.ac.in>
2023-05-19 12:33:34 +0530

    changes done

--- src/jsx/layouts/nav/Menu.js
+++ src/jsx/layouts/nav/Menu.js
@@ -14,11 +14,11 @@ export const MenuList = [
                 to: 'dashboard-dark',                
             },
             {
-                title: 'Guest List',
+                title: 'Customer  List',
                 to: 'guest-list',                
             },
             {
-                title: 'Guest Details',
+                title: 'Customer Details',
                 to: 'guest-details',                
             },
             {
@@ -30,13 +30,17 @@ export const MenuList = [
                 to: 'room-list',                
             },
             {
-                title: 'Reviews',
-                to: 'reviews',                
+                title: 'Room Details',
+                to: 'guest-details',                
             },
             {
-                title: 'Task',
+                title: 'Maintenance',
                 to: 'task',                
-            },			
+            },	
+            {
+                title: 'Reviews',
+                to: 'reviews',                
+            }		
         ],
     },
     
@@ -117,232 +121,5 @@ export const MenuList = [
                 ],
             },
         ],
-    },
-    //Charts
-    {
-        title: 'Charts',	
-        classsChange: 'mm-collapse',
-        iconStyle: <i className="fas fa-chart-line"></i>,
-        content: [
-            
-            {
-                title: 'RechartJs',
-                to: 'chart-rechart',					
-            },
-            {
-                title: 'Chartjs',
-                to: 'chart-chartjs',					
-            },
-            {
-                title: 'Sparkline',
-                to: 'chart-sparkline',					
-            },
-            {
-                title: 'Apexchart',
-                to: 'chart-apexchart',					
-            },
-        ]
-    },
-    //Boosttrap
-    {
-        title: 'Bootstrap',	
-        classsChange: 'mm-collapse',
-        iconStyle: <i className="fab fa-bootstrap"></i>,	
-        content: [
-            {
-                title: 'Accordion',
-                to: 'ui-accordion',					
-            },
-            {
-                title: 'Alert',
-                to: 'ui-alert',					
-            },
-            {
-                title: 'Badge',
-                to: 'ui-badge',					
-            },
-            {
-                title: 'Button',
-                to: 'ui-button',					
-            },
-            {
-                title: 'Modal',
-                to: 'ui-modal',					
-            },
-            {
-                title: 'Button Group',
-                to: 'ui-button-group',					
-            },
-            {
-                title: 'List Group',
-                to: 'ui-list-group',					
-            },
-            {
-                title: 'Cards',
-                to: 'ui-card',					
-            },
-            {
-                title: 'Carousel',
-                to: 'ui-carousel',					
-            },
-            {
-                title: 'Dropdown',
-                to: 'ui-dropdown',					
-            },
-            {
-                title: 'Popover',
-                to: 'ui-popover',					
-            },
-            {
-                title: 'Progressbar',
-                to: 'ui-progressbar',					
-            },
-            {
-                title: 'Tab',
-                to: 'ui-tab',					
-            },
-            {
-                title: 'Typography',
-                to: 'ui-typography',					
-            },
-            {
-                title: 'Pagination',
-                to: 'ui-pagination',					
-            },
-            {
-                title: 'Grid',
-                to: 'ui-grid',					
-            },
-        ]
-    },
-    //plugins
-    {
-        title:'Plugins',
-        classsChange: 'mm-collapse',
-        iconStyle : <i className="fas fa-heart"></i>,
-        content : [
-            {
-                title:'Select 2',
-                to: 'uc-select2',
-            },
-            // {
-            //     title:'Noui Slider',
-            //     to: 'uc-noui-slider',
-            // },
-            {
-                title:'Sweet Alert',
-                to: 'uc-sweetalert',
-            },
-            {
-                title:'Toastr',
-                to: 'uc-toastr',
-            },
-            {
-                title:'Jqv Map',
-                to: 'map-jqvmap',
-            },
-            {
-                title:'Light Gallery',
-                to: 'uc-lightgallery',
-            },
-        ]
-    },
-    //Widget
-    {   
-        title:'Widget',
-        //classsChange: 'mm-collapse',
-        iconStyle: <i className="fas fa-user-check"></i>,
-        to: 'widget-basic',
-    },
-    //Forms
-    {
-        title:'Forms',
-        classsChange: 'mm-collapse',
-        iconStyle: <i className="fas fa-file-alt"></i>,
-        content : [
-            {
-                title:'Form Elements',
-                to: 'form-element',
-            },
-            {
-                title:'Wizard',
-                to: 'form-wizard',
-            },
-            {
-                title:'CkEditor',
-                to: 'form-ckeditor',
-            },
-            {
-                title:'Pickers',
-                to: 'form-pickers',
-            },
-            {
-                title:'Form Validate',
-                to: 'form-validation',
-            },
-
-        ]
-    },
-    //Table
-    {
-        title:'Table',
-        classsChange: 'mm-collapse',
-        iconStyle: <i className="fas fa-table"></i>,
-        content : [
-            {
-                title:'Table Filtering',
-                to: 'table-filtering',
-            },
-            {
-                title:'Table Sorting',
-                to: 'table-sorting',
-            },
-            {
-                title:'Bootstrap',
-                to: 'table-bootstrap-basic',
-            },
-           
-
-        ]
-    },
-    //Pages
-    {
-        title:'Pages',
-        classsChange: 'mm-collapse',
-        iconStyle: <i className="fas fa-clone"></i>,
-        content : [
-            {
-                title:'Error',
-                hasMenu : true,
-                content : [
-                    {
-                        title: 'Error 400',
-                        to : 'page-error-400',
-                    },
-                    {
-                        title: 'Error 403',
-                        to : 'page-error-403',
-                    },
-                    {
-                        title: 'Error 404',
-                        to : 'page-error-404',
-                    },
-                    {
-                        title: 'Error 500',
-                        to : 'page-error-500',
-                    },
-                    {
-                        title: 'Error 503',
-                        to : 'page-error-503',
-                    },
-                ],
-            },
-            {
-                title:'Lock Screen',
-                to: 'page-lock-screen',
-            },
-
-        ]
-    },
-    
+    }
 ]
